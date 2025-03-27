28bd7e6ddd1f279d1094ef87ee7f3f7da8d51c7f
linhpq <linhpq@infinitytech.global>
2025-03-27 14:50:48 +0700

    drum learn and campaign

--- lib/src/mvvm/views/drum_learn/widget/list_category_song_widget.dart
+++ lib/src/mvvm/views/drum_learn/widget/list_category_song_widget.dart
@@ -1,6 +1,8 @@
 import 'package:drumpad_flutter/core/res/drawer/image.dart';
+import 'package:drumpad_flutter/src/mvvm/view_model/drum_learn_provider.dart';
 import 'package:drumpad_flutter/src/mvvm/views/drum_learn/widget/item_category_song.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ListCategorySongWidget extends StatelessWidget {
   const ListCategorySongWidget({super.key});
@@ -11,7 +13,8 @@ class ListCategorySongWidget extends StatelessWidget {
       shrinkWrap: true,
       itemCount: 10,
       itemBuilder: (context, index) {
-      return ItemCategorySong(asset: ResImage.imgRose,);
+        final song = context.read<DrumLearnProvider>().data[index];
+      return ItemCategorySong(model: song);
       },
     );
   }
