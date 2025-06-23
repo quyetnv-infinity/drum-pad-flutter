8d6d92d3989032c9c76348246543ed5d38612a29
linhpq9818 <linhpq@infinitytech.global>
2025-06-23 16:30:55 +0700

    home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,21 @@
+import 'package:and_drum_pad_flutter/service_locator/service_locator.dart';
 import 'package:and_drum_pad_flutter/view/my_application.dart';
 import 'package:and_drum_pad_flutter/view/screen/home/home_screen.dart';
+import 'package:and_drum_pad_flutter/view_model/locale_view_model.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(MyApplication());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await ServiceLocator.instance.initialise();
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => LocateViewModel()),
+      ],
+      child: const MyApplication(),
+    ),
+  );
 }
