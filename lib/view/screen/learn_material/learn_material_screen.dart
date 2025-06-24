cec214936b64457466d33366750a0d3b7e3fdccf
hoanglm6201 <hoanglm@infinitytech.global>
2025-06-24 10:26:55 +0700

    learn material app bar

--- lib/view/screen/learn_material/learn_material_screen.dart
+++ lib/view/screen/learn_material/learn_material_screen.dart
@@ -1,3 +1,6 @@
+import 'package:and_drum_pad_flutter/core/res/drawer/icon.dart';
+import 'package:and_drum_pad_flutter/core/utils/locator_support.dart';
+import 'package:and_drum_pad_flutter/view/widget/app_bar/custom_app_bar.dart';
 import 'package:and_drum_pad_flutter/view/widget/scaffold/custom_scaffold.dart';
 import 'package:flutter/material.dart';
 
@@ -13,7 +16,13 @@ class _LearnMaterialScreenState extends State<LearnMaterialScreen> {
   @override
   Widget build(BuildContext context) {
     return AppScaffold(
-
+      appBar: CustomAppBar(
+        iconLeading: ResIcon.icBack,
+        onTapLeading: () {
+          Navigator.pop(context);
+        },
+        title: context.locale.learn_material,
+      ),
     );
   }
 }
