7580c743109a524d7deecebdb06bafd811a9ffb0
hoanglm6201 <hoanglm@infinitytech.global>
2025-06-24 10:40:07 +0700

    recommend list song learn material

--- lib/view/screen/learn_material/learn_material_screen.dart
+++ lib/view/screen/learn_material/learn_material_screen.dart
@@ -1,8 +1,11 @@
 import 'package:and_drum_pad_flutter/core/res/drawer/icon.dart';
 import 'package:and_drum_pad_flutter/core/utils/locator_support.dart';
+import 'package:and_drum_pad_flutter/view/screen/beat_runner/widget/recommend_list_song.dart';
 import 'package:and_drum_pad_flutter/view/widget/app_bar/custom_app_bar.dart';
 import 'package:and_drum_pad_flutter/view/widget/scaffold/custom_scaffold.dart';
+import 'package:and_drum_pad_flutter/view_model/drum_learn_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class LearnMaterialScreen extends StatefulWidget {
   const LearnMaterialScreen({super.key});
@@ -22,6 +25,29 @@ class _LearnMaterialScreenState extends State<LearnMaterialScreen> {
           Navigator.pop(context);
         },
         title: context.locale.learn_material,
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0).copyWith(right: 0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Consumer<DrumLearnProvider>(
+                builder: (context, drumLearnProvider, _) {
+                  return drumLearnProvider.listRecommend.isEmpty ? Container() : Flexible(
+                    child: RecommendListSong(
+                      title: context.locale.recommend_list_songs,
+                      listSongs: drumLearnProvider.listRecommend,
+                      onTapItem: () {
+
+                      },
+                    )
+                  );
+                }
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
