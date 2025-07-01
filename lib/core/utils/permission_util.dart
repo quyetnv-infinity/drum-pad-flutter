fd8d6cf743aabae0d6c87379de2b973e4d10b532
hoanglm6201 <hoanglm@infinitytech.global>
2025-07-01 14:46:42 +0700

    permission dialog

--- lib/core/utils/permission_util.dart
+++ lib/core/utils/permission_util.dart
@@ -4,32 +4,34 @@ import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 class PermissionUtil {
-  static void showRequestPhotosPermissionDialog(BuildContext context){
-    // RequestPermissionDialog(
-    //     title: context.locale.photo_permission,
-    //     content: context.locale.photo_permission_description,
-    //     acceptText: context.locale.grant_permission,
-    //     onAcceptPressed: () async {
-    //       AdController.shared.setResumeAdState(true);
-    //       await openAppSettings();
-    //     },
-    //     onCompleted: (){
-    //       AdController.shared.setResumeAdState(false);
-    //     }
-    // );
+  static void showRequestPhotosAddOnlyPermissionDialog(BuildContext context){
+    RequestPermissionDialog(
+      title: context.locale.photo_permission,
+      content: context.locale.photo_permission_description,
+      acceptText: context.locale.grant_permission,
+      onAcceptPressed: () async {
+        // AdController.shared.setResumeAdState(true);
+        await openAppSettings();
+      },
+      onCompleted: (){
+        // AdController.shared.setResumeAdState(false);
+      }
+    ).show(context);
   }
-  static void showRequestScreenRecordPermissionDialog(BuildContext context){
-    // RequestPermissionDialog(
-    //     title: context.locale.record_permission,
-    //     content: context.locale.record_permission_description,
-    //     acceptText: context.locale.grant_permission,
-    //     onAcceptPressed: () async {
-    //       AdController.shared.setResumeAdState(true);
-    //       await openAppSettings();
-    //     },
-    //     onCompleted: (){
-    //       AdController.shared.setResumeAdState(false);
-    //     }
-    // );
+
+  Future<void> handleOnTogglePhotosAddOnlyPermission(BuildContext context, {required Function() whenPermissionGranted}) async {
+    final status = await Permission.photosAddOnly.status;
+    if (!status.isGranted) {
+      await Permission.photosAddOnly.request();
+      final requestStatus = await Permission.photosAddOnly.status;
+      if(requestStatus.isPermanentlyDenied){
+        if(!context.mounted) return;
+        showRequestPhotosAddOnlyPermissionDialog(context);
+      } else if (requestStatus.isGranted) {
+        whenPermissionGranted();
+      }
+    } else {
+      whenPermissionGranted();
+    }
   }
 }
