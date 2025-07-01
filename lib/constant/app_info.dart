5330f49e44a386992edcf4498c1c6be99ad9b2f3
hoanglm6201 <hoanglm@infinitytech.global>
2025-07-01 14:24:46 +0700

    link policy & app link

--- lib/constant/app_info.dart
+++ lib/constant/app_info.dart
@@ -1,7 +1,9 @@
 class AppInfo {
-  static const String termOfServiceLink = 'https://sites.google.com/view/term-of-service-nh-app/trang-chủ';
-  static const String policyPrivacyLink = 'https://sites.google.com/view/policyprivacy-nh-app/trang-chủ';
+  static const String termOfServiceLink = 'https://sites.google.com/view/gampolicy2023';
+  static const String policyPrivacyLink = 'https://sites.google.com/view/gampolicy2023';
   static const String appId = '6744993456';
-  static const String appLink = 'https://apps.apple.com/app/id$appId';
+  static const String packageName = 'com.beat.maker.drum.beatapp';
+  static const String appLinkIOS = 'https://apps.apple.com/app/id$appId';
+  static const String appLinkAndroid = 'https://play.google.com/store/apps/details?id=$packageName';
   static const String appIcon = 'assets/images/app_icon.png';
 }
